b356d200b5e28b1a54a6d987dba7baab9456592d
Truong <Phuc.Truong@bluescope.com>
2020-03-26 13:31:46 +0700

    fix view in course

--- resources/views/v1/fontend/course/detail/detail.blade.php
+++ resources/views/v1/fontend/course/detail/detail.blade.php
@@ -13,6 +13,44 @@ h2, p {
   margin-top: 5px;
   margin-bottom: 5px !important;
 }
+
+button.like{
+  width: 30px;
+  height: 30px;
+  margin: 0 auto;
+  line-heigth: 50px;
+  border-radius: 50%;
+  color: rgba(0,150,136 ,1);
+  background-color:rgba(38,166,154 ,0.3);
+  border-color: rgba(0,150,136 ,1);
+  border-width: 1px;
+  font-size: 15px;
+}
+
+button.dislike{
+  width: 30px;
+  height: 30px;
+  margin: 0 auto;
+  line-heigth: 50px;
+  border-radius: 50%;
+  color: rgba(255,82,82 ,1);
+  background-color: rgba(255,138,128 ,0.3);
+  border-color: rgba(255,82,82 ,1);
+  border-width: 1px;
+  font-size: 15px;
+}
+
+button.learnmore{
+  width: 100%;
+  padding: 10px;
+  border: none;
+  background: rgba(0,151,167 ,1);
+  border-radius: 5px;
+  text-transform: uppercase;
+  font-size: 16px;
+  color: #fff;
+  letter-spacing: 1px;
+}
 </style>
 
 @endsection
@@ -49,7 +87,7 @@ h2, p {
         <section id="line{{$val->id}}">
             <div class="row">
                 <div class="col-md-12 left-align">
-                    <h2 class="dark-text">{{ $val->title }} <a href="#top">#back to top</a><hr></h2>
+                    <h2 class="dark-text">{{ $val->title }} <a href="{{ url()->current() }}#top">#back to top</a><hr></h2>
                 </div>
                 <!-- end col -->
             </div>
@@ -63,6 +101,30 @@ h2, p {
             </div>
         </section>
         @endforeach
+
+        
+        @if(Auth::check())
+        <hr>
+        <section id="">
+          <div class="row">
+            <div class="col-md-12 left-align">
+              Cảm ơn {{ Auth::user()->name }} đã xem bài {{ $lesson->name }} trong khóa học {{ $info->name }} <br>
+              Did you like this lesson?
+              <button class="dislike">
+                <i class="fa fa-thumbs-o-down" aria-hidden="true"></i>
+              </button>
+
+              <button class="like">
+                <i class="fa fa-thumbs-o-up" aria-hidden="true"></i>
+              </button>
+
+            </div>
+
+          </div>
+        </section>
+        @endif
+        <hr>
+        <!-- end section -->
 
         <section id="lineCopy">
           <div class="row">
@@ -78,8 +140,9 @@ h2, p {
             </div>
           </div>
         </section>
-          <!-- end section -->
       </div>
+
+
         <!-- // end .col -->
 
     </div>
